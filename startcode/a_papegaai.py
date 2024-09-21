2bd7f7b3ef41907ad3112f45c5f9827de0149293
Wlouis2010 <louiswathiong@hotmail.com>
2024-09-21 12:18:18 +0200

    oefening papegaai

--- startcode/a_papegaai.py
+++ startcode/a_papegaai.py
@@ -1,2 +1,9 @@
 # Schrijf een Python-programma dat de gebruiker vraagt om een tekst in te voeren en deze tekst vervolgens afdrukt.
 # Kies een gepaste naam voor je variabele.
+
+print('hello world')
+naam = input("Wat is je naam? ")
+print("Hallo, hoe gaat het met jou " + naam + "?")
+leeftijd = int(input("Wat is je leeftijd? "))
+
+print("Volgend jaar ben je ",  leeftijd + 1 )
--- startcode/b_concerttickets.py
+++ startcode/b_concerttickets.py
@@ -10,3 +10,7 @@
 #
 # Voorbeelduitvoer:
 # De totale prijs bedraagt 31.65 euro.
+print('hallo, welkom')
+prijs = int(input("Wat is je de prijs van je tickets in euro's? "))
+Aantal = int(input("Hoeveel tickets wil je kopen? "))
+print("je moet ",   prijs * Aantal , "euro betalen")
